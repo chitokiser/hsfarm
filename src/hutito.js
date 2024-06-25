47937e5789ad6021f267b1da95a84650f9ffa152
daguri <daguri75@gmail.com>
2024-06-25 20:56:37 +0900

    1

---
+++ src/hutito.js
@@ -0,0 +1,225 @@
+let hutaddr = {  
+    hutito: "0x68bE39Dbcf950b36B061Fd5a50CCAD9d08D07299" //hutito
+
+  };
+
+  let hutabi = {
+  
+    hutito: [
+   
+ 
+       "function g1() public view virtual returns(uint256)",
+       "function g2() public view virtual returns(uint256)",
+       "function g4() public view virtual returns(uint256) ",
+       "function rate() public view virtual returns(uint256) ", //토큰할인율
+       "function memberjoin(address _mento) public ",
+       "function offering(uint _num) public",
+       "function withdraw()public",
+       "function getMenty(address user) public view returns (address[] memory)",
+       "function myinfo(address user) public view virtual returns(address,uint256,bool) "
+      ],
+      
+ 
+
+  };
+
+  let itotopSync = async () => {
+
+    let provider = new ethers.providers.JsonRpcProvider('https://opbnb-mainnet-rpc.bnbchain.org');
+    let meta5Contract = new ethers.Contract(hutaddr.hutito, hutabi.hutito, provider);
+  
+    
+
+    let icya = await meta5Contract.g1();  
+    let ihut = await meta5Contract.g2();
+    let irate = await meta5Contract.rate();
+    let iprice = await meta5Contract.g4();
+   
+    document.getElementById("Cyabalito").innerHTML= (icya/1e18).toFixed(2);   
+    document.getElementById("Hutbalito").innerHTML= (ihut);   
+    document.getElementById("Hutrate").innerHTML= (100-irate);   
+    document.getElementById("Hprice").innerHTML= (iprice/1e18).toFixed(4);   
+    }
+   
+
+ // 호출 코드
+ itotopSync();
+  
+
+
+
+ let Hutmemberjoin  = async () => {
+   
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let meta5Contract = new ethers.Contract(hutaddr.hutito, hutabi.hutito, signer);
+  try {
+    await meta5Contract.memberjoin(document.getElementById('hutmento').value);
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+
+};
+
+let Hutoper  = async () => {
+   
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let meta5Contract = new ethers.Contract(hutaddr.hutito, hutabi.hutito, signer);
+  try {
+    await meta5Contract.offering(document.getElementById('hutAmount').value);
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+
+};
+
+let HutLogin = async () => {
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+  let meta5Contract = new ethers.Contract(hutaddr.hutito, hutabi.hutito, signer);
+
+  let my = await meta5Contract.myinfo(await signer.getAddress());
+  let imento =  (await my[0]);
+  let idepo =  (await my[1]);
+  let icerti =  (await my[2]);
+  document.getElementById("Mymento").innerHTML = (imento);
+  document.getElementById("Myfee").innerHTML = (idepo/2e18);
+  document.getElementById("Mentobool").innerHTML =  (icerti);
+ 
+};
+
+
+
+
+
+let Withdraw = async () => {
+   
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let meta5Contract = new ethers.Contract(hutaddr.hutito, hutabi.hutito, signer);
+  try {
+    await meta5Contract.withdraw(); 
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+
+};
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  let Getmymenty = async () => {
+      try {
+          let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+
+          await window.ethereum.request({
+              method: "wallet_addEthereumChain",
+              params: [{
+                  chainId: "0xCC",
+                  rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+                  chainName: "opBNB",
+                  nativeCurrency: {
+                      name: "BNB",
+                      symbol: "BNB",
+                      decimals: 18
+                  },
+                  blockExplorerUrls: ["https://opbnbscan.com"]
+              }]
+          });
+
+          await userProvider.send("eth_requestAccounts", []);
+          let signer = userProvider.getSigner();
+
+          console.log("Signer obtained:", signer);
+
+          let meta5Contract = new ethers.Contract(hutaddr.hutito, hutabi.hutito, signer);
+
+          // 사용자의 주소를 가져옵니다.
+          let userAddress = await signer.getAddress();
+          console.log("User Address:", userAddress);
+          
+          // getMenty 함수를 호출합니다.
+          let menties = await meta5Contract.getMenty(userAddress); 
+          console.log("Menties obtained:", menties);
+          
+          // 결과를 DOM에 표시합니다.
+          let mentiesList = document.getElementById("Mymenty");
+          mentiesList.innerHTML = ""; // 기존 내용을 지웁니다
+
+          menties.forEach((item) => {
+              let listItem = document.createElement("li");
+              listItem.textContent = item;
+              mentiesList.appendChild(listItem);
+          });
+      } catch(e) {
+          console.error(e);
+          alert(e.message ? e.message : 'An unknown error occurred');
+      }
+  };
+
+  // 버튼 클릭 이벤트 핸들러 추가
+  document.getElementById("getMentyButton").addEventListener("click", Getmymenty);
+});
---
+++ src/mypage.js
@@ -0,0 +1,243 @@
+let contractAddress = {
+  metbank: "0xc490E20CFdF817B5412D58864226e01560dA37a8",  //hutbnak
+  metmarket: "0x87280638619c508967c42AE10c0c3E6d5d57f2c0" //sutrealmarket
+    
+  };
+  let contractAbi = {
+   
+    metmarket: [
+      "function buy(uint _mid) public",
+      "function mid() public view returns (uint256)",
+      "function tax() public view returns (uint256)",
+      "function g1() public view virtual returns(uint256)",
+      "function selladd(uint _mid,uint256 _init) public",
+      "function getmainpass(uint _mid) external view returns (string memory)",
+      "function getpass(uint256 _mid) external view returns (string memory)",  //관람자패스
+      "function getmetainfo(uint _num) public view returns (uint256, uint256, string memory, uint256,uint8, address,address) ",
+      "function charge(uint _pay) public",
+      "function newmeta(uint _metanum,string memory _investor,uint256 _init,string memory _mainpass) public"
+     ],
+   
+    metbank: [
+      "function g1() public view virtual returns(uint256)",
+      "function g6() public view virtual returns(uint256)",
+      "function g7() public view virtual returns(uint256)",
+      "function g8(address user) public view virtual returns(uint256)",
+      "function g9(address user) public view returns(uint)",
+      "function g10() public view virtual returns(uint256)",
+      "function allow() public view returns(uint256)",
+      "function sum() public view returns(uint256)",
+      "function allowt(address user) public view returns(uint256)",
+      "function g11() public view virtual returns(uint256)",
+      "function getprice() public view returns (uint256)",
+      "function gettime() external view returns (uint256)",
+      "function withdraw() public ",
+      "function buysut(uint _num) public returns(bool)",
+      "function sellsut(uint num)public returns(bool)",
+      "function getpay(address user) public view returns (uint256)",
+      "function allowcation() public returns(bool) ",
+      "function getlevel(address user) public view returns(uint) ",
+      "function getmento(address user) public view returns(address) ",
+      "function getagent(address user) public view returns(address) ",
+      "function memberjoin(address _mento) public ",
+      "function myinfo(address user) public view returns(uint256,uint256,uint256,address,address,uint256,uint256)",
+      "function levelup() public"
+
+    ]
+
+
+  };
+
+
+
+  let MemberLogin = async () => {
+    let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [{
+          chainId: "0xCC",
+          rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+          chainName: "opBNB",
+          nativeCurrency: {
+              name: "BNB",
+              symbol: "BNB",
+              decimals: 18
+          },
+          blockExplorerUrls: ["https://opbnbscan.com"]
+      }]
+  });
+    await userProvider.send("eth_requestAccounts", []);
+    let signer = userProvider.getSigner();
+    let cyamemContract = new ethers.Contract(contractAddress.metbank, contractAbi.metbank, signer);
+    let mylev = parseInt(await cyamemContract.getlevel(await signer.getAddress()));
+    let mymento = (await cyamemContract.getmento(await signer.getAddress()));  
+    let levelexp = (2**mylev)*10000;
+    let my = await cyamemContract.myinfo(await signer.getAddress());
+    let myexp =  (await my[6]);
+    let mybonus =  (await my[1]);
+    document.getElementById("Mylev").innerHTML = (mylev);
+    document.getElementById("Mylev2").innerHTML = (mylev);
+    document.getElementById("Exp").innerHTML =  (myexp);
+    document.getElementById("Expneeded").innerHTML = (levelexp);
+    document.getElementById("Mypoint").innerHTML =  (mybonus/1e18).toFixed(4);
+    document.getElementById("Mymento").innerHTML = (mymento);
+    
+    document.getElementById("LevelBar").style.width = `${myexp/levelexp*100}%`; // CHECK:: 소수점으로 나오는 것 같아 *100 했습니다. 
+
+
+  };
+
+  let Levelup = async () => {
+   
+    let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+    await window.ethereum.request({
+      method: "wallet_addEthereumChain",
+      params: [{
+          chainId: "0xCC",
+          rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+          chainName: "opBNB",
+          nativeCurrency: {
+              name: "BNB",
+              symbol: "BNB",
+              decimals: 18
+          },
+          blockExplorerUrls: ["https://opbnbscan.com"]
+      }]
+  });
+    await userProvider.send("eth_requestAccounts", []);
+    let signer = userProvider.getSigner();
+
+    let cyamemContract = new ethers.Contract(contractAddress.metbank, contractAbi.metbank, signer);
+    try {
+      await cyamemContract.levelup(); 
+    } catch(e) {
+      alert(e.data.message.replace('execution reverted: ',''))
+    }
+  
+};
+
+
+
+
+let Bonuswithdraw = async () => {
+   
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let cyamemContract = new ethers.Contract(contractAddress.metbank, contractAbi.metbank, signer);
+  
+  try {
+    await cyamemContract. withdraw(); 
+    //await cyabankContract.buycut(document.getElementById('buyAmount').value);
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+
+};
+
+
+let Withdraw = async () => {
+   
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let cutdefiContract = new ethers.Contract(contractAddress.cutdefiAddr, contractAbi.cutdefi, signer);
+  
+  try {
+    await cutdefiContract. withdraw(); //cutdefi 멘토수당
+    //await cyabankContract.buycut(document.getElementById('buyAmount').value);
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+
+};
+
+let Mentolevelup = async () => {
+   
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let cyamemContract = new ethers.Contract(contractAddress.cyamemAddr, contractAbi.cyamem, signer);
+  
+  try {
+    await cyamemContract. mentolevelup(); //cat 개수 필요
+    //await cyabankContract.buycut(document.getElementById('buyAmount').value);
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+
+};
+
+
+let Charge = async () => {
+  let userProvider = new ethers.providers.Web3Provider(window.ethereum, "any");
+  await window.ethereum.request({
+    method: "wallet_addEthereumChain",
+    params: [{
+        chainId: "0xCC",
+        rpcUrls: ["https://opbnb-mainnet-rpc.bnbchain.org"],
+        chainName: "opBNB",
+        nativeCurrency: {
+            name: "BNB",
+            symbol: "BNB",
+            decimals: 18
+        },
+        blockExplorerUrls: ["https://opbnbscan.com"]
+    }]
+});
+  await userProvider.send("eth_requestAccounts", []);
+  let signer = userProvider.getSigner();
+
+  let meta5Contract = new ethers.Contract(contractAddress.metmarket, contractAbi.metmarket, signer);
+
+  try {
+    await meta5Contract.charge(document.getElementById('chargeAmount').value);
+  } catch(e) {
+    alert(e.data.message.replace('execution reverted: ',''))
+  }
+};
